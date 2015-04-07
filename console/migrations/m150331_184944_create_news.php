c53dbbc1b2d0bd24b6b11643349b2f2a5fabcc91
Bayu Suwarning Hadi <bayusuwarninghadi@gmail.com>
2015-04-07 21:13:40 +0700

    improvement

---
+++ console/migrations/m150331_184944_create_news.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150331_184944_create_news extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m150331_184944_create_news cannot be reverted.\n";
+
+        return false;
+    }
+    
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+    
+    public function safeDown()
+    {
+    }
+    */
+}
