4211fd6d62807a1d08aa5002ba6b8acf7989d1ac
Bayu Suwarning Hadi <bayusuwarninghadi@gmail.com>
2015-04-16 04:04:19 +0700

    Fixed tree hover behaviour

--- backend/views/category/_list.php
+++ backend/views/category/_list.php
@@ -7,24 +7,35 @@ use yii\helpers\Html;
 <ul class="nav categories-tree" role="navigation">
 	<?php foreach ($model as $category) : ?>
 		<li>			
-			<div class="btn-group btn-group-sm hide">
-		        <?= Html::a('<i class="fa fa-users fa-fw"></i> Add Child', ['create', 'prepend' => $category->id], ['class' => 'btn btn-success'])?>
-		        <?= Html::a('<i class="fa fa-user fa-fw"></i> Insert After', ['create', 'after' => $category->id], ['class' => 'btn btn-success'])?>
-		        <?= Html::a('<i class="fa fa-pencil fa-fw"></i> Edit', ['edit', 'id' => $category->id], ['class' => 'btn btn-warning'])?>
-		        <?= Html::a('<i class="fa fa-trash fa-fw"></i> Delete', ['delete', 'id' => $category->id], [
-		            'class' => 'btn btn-danger',
-		            'data' => [
-		                'confirm' => 'Are you sure you want to delete this item?',
-		                'method' => 'post',
-		            ],
-		        ]) ?>
-			</div>
-
 			<?php if ($_child = $category->children(1)->all()) : ?>
 				<a href="#"><i class="fa fa-th-large fa-fw"></i> <?=$category->name?> <span class="fa arrow"></span></a>
+				<div class="btn-group btn-group-sm hide">
+			        <?= Html::a('<i class="fa fa-users fa-fw"></i> Add Child', ['create', 'prepend' => $category->id], ['class' => 'btn btn-success'])?>
+			        <?= Html::a('<i class="fa fa-user fa-fw"></i> Insert After', ['create', 'after' => $category->id], ['class' => 'btn btn-success'])?>
+			        <?= Html::a('<i class="fa fa-pencil fa-fw"></i> Edit', ['update', 'id' => $category->id], ['class' => 'btn btn-warning'])?>
+			        <?= Html::a('<i class="fa fa-trash fa-fw"></i> Delete', ['delete', 'id' => $category->id], [
+			            'class' => 'btn btn-danger',
+			            'data' => [
+			                'confirm' => 'Are you sure you want to delete this item?',
+			                'method' => 'post',
+			            ],
+			        ]) ?>
+				</div>
 				<?= $this->render('_list', ['model' => $_child])?>
 			<?php else : ?>
 				<a href="#"><i class="fa fa-th-large fa-fw"></i> <?=$category->name?></a>
+				<div class="btn-group btn-group-sm hide">
+			        <?= Html::a('<i class="fa fa-users fa-fw"></i> Add Child', ['create', 'prepend' => $category->id], ['class' => 'btn btn-success'])?>
+			        <?= Html::a('<i class="fa fa-user fa-fw"></i> Insert After', ['create', 'after' => $category->id], ['class' => 'btn btn-success'])?>
+			        <?= Html::a('<i class="fa fa-pencil fa-fw"></i> Edit', ['update', 'id' => $category->id], ['class' => 'btn btn-warning'])?>
+			        <?= Html::a('<i class="fa fa-trash fa-fw"></i> Delete', ['delete', 'id' => $category->id], [
+			            'class' => 'btn btn-danger',
+			            'data' => [
+			                'confirm' => 'Are you sure you want to delete this item?',
+			                'method' => 'post',
+			            ],
+			        ]) ?>
+				</div>
 			<?php endif ?>
 		</li>
 	<?php endforeach ?>
