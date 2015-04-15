8ca0904713f7fd1e99090338e867f39fbf97a1ef
Bayu Suwarning Hadi <bayusuwarninghadi@gmail.com>
2015-04-16 03:53:21 +0700

    Category Tree Action

--- backend/views/category/_list.php
+++ backend/views/category/_list.php
@@ -6,7 +6,20 @@ use yii\helpers\Html;
 ?>
 <ul class="nav categories-tree" role="navigation">
 	<?php foreach ($model as $category) : ?>
-		<li>
+		<li>			
+			<div class="btn-group btn-group-sm hide">
+		        <?= Html::a('<i class="fa fa-users fa-fw"></i> Add Child', ['create', 'prepend' => $category->id], ['class' => 'btn btn-success'])?>
+		        <?= Html::a('<i class="fa fa-user fa-fw"></i> Insert After', ['create', 'after' => $category->id], ['class' => 'btn btn-success'])?>
+		        <?= Html::a('<i class="fa fa-pencil fa-fw"></i> Edit', ['edit', 'id' => $category->id], ['class' => 'btn btn-warning'])?>
+		        <?= Html::a('<i class="fa fa-trash fa-fw"></i> Delete', ['delete', 'id' => $category->id], [
+		            'class' => 'btn btn-danger',
+		            'data' => [
+		                'confirm' => 'Are you sure you want to delete this item?',
+		                'method' => 'post',
+		            ],
+		        ]) ?>
+			</div>
+
 			<?php if ($_child = $category->children(1)->all()) : ?>
 				<a href="#"><i class="fa fa-th-large fa-fw"></i> <?=$category->name?> <span class="fa arrow"></span></a>
 				<?= $this->render('_list', ['model' => $_child])?>
--- backend/views/category/index.php
+++ backend/views/category/index.php
@@ -16,6 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Create Category', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
     <div class="panel panel-primary">
+        <div class="panel-heading"><i class="fa fa-list fa-fw"></i> Category Tree</div>
         <div class="category-index-tree">
             <?=$this->render('_list',[ 'model' => $model ])?>
         </div>
