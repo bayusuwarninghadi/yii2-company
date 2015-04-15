2f62beb4b9d2af42123d09e3fff7ed5e7b7a5f14
Bayu Suwarning Hadi <bayusuwarninghadi@gmail.com>
2015-04-15 10:43:56 +0700

    Adding simple inbox manager

--- backend/views/layouts/_navigation.php
+++ backend/views/layouts/_navigation.php
@@ -18,29 +18,23 @@ echo Nav::widget([
     'options' => ['class' => 'nav navbar-top-links navbar-right'],
     'items' => [
         [
-            'label' => '<i class="fa fa-gear fa-fw"></i>',
-            'url' => ['/setting/index']
-        ],
-        [
-            'label' => '<i class="fa fa-envelope fa-fw"></i>',
-            'items' => [
-                '<li class="divider"></li>',
-                [
-                    'label' => 'See All Inbox',
-                    'url' => ['/inbox/index']
-                ]
-            ]
-        ],
-        [
             'label' => '<i class="fa fa-user fa-fw"></i>',
             'items' => [
                 [
-                    'label' => Yii::$app->user->identity->username,
-                    'url' => ['/user/view?id='.Yii::$app->user->getId()],
+                    'label' => '<i class="fa fa-user fa-fw"></i> ' . Yii::$app->user->identity->username,
+                    'url' => ['/user/view', 'id' => Yii::$app->user->getId()],
+                ],
+                [
+                    'label' => '<i class="fa fa-envelope fa-fw"></i> Inbox',
+                    'url' => ['/inbox/index']
+                ],
+                [
+                    'label' => '<i class="fa fa-gear fa-fw"></i> Setting',
+                    'url' => ['/setting/index']
                 ],
                 '<li class="divider"></li>',    
                 [
-                    'label' => 'Logout',
+                    'label' => '<i class="fa fa-sign-out fa-fw"></i>  Logout',
                     'url' => ['/site/logout'],
                     'linkOptions' => ['data-method' => 'post']
                 ]
