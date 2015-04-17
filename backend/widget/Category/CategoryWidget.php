608ac216a52cb41ac45c8bac7cb498605d8f08cc
Bayu Suwarning Hadi <bayusuwarninghadi@gmail.com>
2015-04-17 13:56:24 +0700

    Move Category widget

---
+++ backend/widget/Category/CategoryWidget.php
@@ -0,0 +1,116 @@
+<?php
+namespace backend\widget\category;
+
+use common\models\Category;
+use yii\helpers\Html;
+use yii\widgets\InputWidget;
+
+/**
+ * Class CategoryWidget
+ * @package backend\widget
+ */
+class CategoryWidget extends InputWidget
+{
+
+    /**
+     * @var bool
+     */
+    public $withPanel = true;
+    /**
+     * @var bool
+     */
+    public $renderOption = false;
+
+    /**
+     * Initializes the widget.
+     */
+    public function init()
+    {
+        CategoryWidgetAssets::register($this->view);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function run()
+    {
+        if ($this->withPanel) {
+            echo Html::beginTag('div', ['class' => 'panel panel-primary']);
+            echo Html::tag('div', '<b>Select Category</b>', ['class' => 'panel-heading']);
+            static::runElement();
+            echo Html::endTag('div');
+        } else {
+            static::runElement();
+        }
+    }
+
+    /**
+     * run main element
+     */
+    protected function runElement()
+    {
+        $id = $this->getId();
+
+        echo Html::beginTag('div', ['class' => 'category-tree-container', 'id' => $id]);
+        static::renderCategory(Category::find()->roots()->all());
+        echo Html::activeHiddenInput($this->model, $this->attribute);
+        echo Html::endTag('div');
+
+        $view = $this->getView();
+        $view->registerCss(".category-tree-container a { border-top:1px solid #ccc }");
+        $view->registerJs("jQuery('#$id>ul').metisMenu();");
+
+        $input_id = Html::getInputId($this->model, $this->attribute);
+        $view->registerJs("
+			jQuery('.category-tree-container a').click(function(){
+				jQuery('#$input_id').val($(this).data('id'))
+			});");
+    }
+
+    /**
+     * @param Category[] $categories
+     * @param int $level
+     */
+    protected function renderCategory($categories, $level = 1)
+    {
+        echo Html::beginTag('ul', ['class' => 'nav categories-tree', 'role' => 'navigation']);
+        foreach ($categories as $category) {
+            echo Html::beginTag('li');
+            $paddingLeft = $level * 15;
+            if ($_child = $category->children(1)->all()) {
+                echo Html::a('+ ' . $category->name . ' <i class="fa arrow"></i>', '#', ['data-id' => $category->id, 'style' => 'padding-left:' . $paddingLeft . 'px']);
+                if ($this->renderOption) {
+                    static::renderOption($category);
+                }
+                static::renderCategory($_child, $level + 1);
+            } else {
+                echo Html::a('+ ' . $category->name, '#', ['data-id' => $category->id, 'style' => 'padding-left:' . $paddingLeft . 'px']);
+                if ($this->renderOption) {
+                    static::renderOption($category);
+                }
+            }
+            echo Html::endTag('li');
+        }
+        echo Html::endTag('ul');
+    }
+
+    /**
+     * @param $category
+     */
+    protected function renderOption($category)
+    {
+        echo Html::beginTag('div', ['class' => 'btn-group btn-group-sm hide']);
+        echo Html::a('Add Child', ['create-category', 'prepend' => $category->id], ['class' => 'btn btn-primary']);
+        echo Html::a('Insert After', ['create-category', 'after' => $category->id], ['class' => 'btn btn-success']);
+        echo Html::a('Edit', ['update-category', 'id' => $category->id], ['class' => 'btn btn-warning']);
+        echo Html::a('Delete', ['delete-category', 'id' => $category->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]);
+        echo Html::endTag('div');
+    }
+
+}
---
+++ backend/widget/Category/CategoryWidgetAssets.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace backend\widget\category;
+
+use yii\web\AssetBundle;
+
+/**
+ * Class CategoryWidgetAssets
+ * @package backend\widget\category
+ */
+class CategoryWidgetAssets extends AssetBundle
+{
+    /**
+     * @var string
+     */
+    public $sourcePath = '@backend/widget/category';
+    /**
+     * @var array
+     */
+    public $css = [
+        'style.css',
+    ];
+}
