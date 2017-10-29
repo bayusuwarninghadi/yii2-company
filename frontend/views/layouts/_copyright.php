9afb0aadf0ec333a093836d3ea799313c2569886
Bayu Suwarning Hadi <bayusuwarninghadi@gmail.com>
2017-10-29 19:58:38 +0700

    Contact

--- frontend/views/layouts/_copyright.php
+++ frontend/views/layouts/_copyright.php
@@ -1,4 +1,7 @@
 <?php
+use yii\helpers\Html;
+use yii\helpers\Url;
+
 /**
  * Created by PhpStorm.
  * User: bay_oz
@@ -10,6 +13,10 @@
 	<div class="container">
 		<div class="col-md-12">
 			2017 <?=Yii::$app->controller->settings['site_name']?>
+            <span class="pull-right">
+                <?=Yii::t('app','Language')?>
+                <?=(Yii::$app->language == 'id-ID') ? Html::a('EN', Url::current(['lang' => 'en-US'])) : Html::a('ID', Url::current(['lang' => 'id-ID']));?>
+            </span>
 		</div>
 	</div>
 </div>
--- frontend/views/site/contact.php
+++ frontend/views/site/contact.php
@@ -1,4 +1,7 @@
 <?php
+use yii\helpers\Html;
+use yii\widgets\Breadcrumbs;
+
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model \frontend\models\ContactForm */
@@ -7,6 +10,21 @@
 $this->title = \Yii::t('app', 'Contact Us');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<div id="heading-breadcrumbs">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-7">
+				<h1><?= Html::encode($this->title) ?></h1>
+			</div>
+			<div class="col-md-5">
+				<?= Breadcrumbs::widget([
+					'links' => $this->params['breadcrumbs'],
+				]); ?>
+			</div>
+		</div>
+	</div>
+</div>
+
 <?= $this->render('_contact', [
 	'model' => $model,
 	'contactPopup' => $contactPopup,
